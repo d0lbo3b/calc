8a433e42e36b92f703f0434d8d46ec23393081ce
k0n4eniyD0lbo3b <slowmanart@gmail.com>
2025-05-03 18:14:42 +0300

    Reapply "Initial commit"
    This reverts commit e00d91f56a55bc26286eea43bd8539a4729a0b79.

---
+++ src/api/Parser.cs
@@ -0,0 +1,5 @@
+﻿namespace calculator.api;
+
+public abstract class Parser {
+    public abstract bool Parse(List<string> input, out string result);
+}
---
+++ src/attributes/AttributeUnwrapper.cs
@@ -0,0 +1,12 @@
+﻿namespace calculator.attributes;
+
+public static class AttributeUnwrapper {
+    public static TA? Unwrap<TA>(Enum enumVal) {
+        var type = enumVal.GetType();
+        var memInfos = type.GetMember(enumVal.ToString());
+        var attributes = memInfos[0].GetCustomAttributes(typeof(TA), false);
+        
+        return attributes.Length > 0 ? (TA)attributes[0] : default;
+    }
+
+}
